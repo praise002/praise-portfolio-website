500fae1483832df3fb979b653833afcebb52c031
Praise Idowu <ifeoluwapraise02@gmail.com>
2025-01-03 13:13:58 +0100

    feat: Implement form submission with Toastify notifications and add CSS for toast messages

---
+++ dist/index.fc2f7771.js
@@ -0,0 +1,74 @@
+const form = document.getElementById("contact-form");
+async function handleSubmit(event) {
+    event.preventDefault();
+    const status = document.getElementById("my-form-status");
+    const submitButton = form.querySelector("#submit-btn");
+    const data = new FormData(event.target);
+    console.log(data);
+    console.log(event.target.action);
+    submitButton.disabled = true;
+    submitButton.textContent = "Submitting...";
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            Accept: "application/json"
+        }
+    }).then((response)=>{
+        if (response.ok) {
+            Toastify({
+                text: "Thank you for your message!",
+                duration: 3000,
+                gravity: "top",
+                position: "right",
+                style: {
+                    background: "#4CAF50"
+                }
+            }).showToast();
+            form.reset();
+        } else response.json().then((data)=>{
+            if (Object.hasOwn(data, "errors")) status.innerHTML = data["errors"].map((error)=>error["message"]).join(", ");
+            else Toastify({
+                text: "Oops! There was a problem submitting your form",
+                duration: 3000,
+                gravity: "top",
+                position: "right",
+                style: {
+                    background: "#F44336"
+                }
+            }).showToast();
+        });
+    }).catch((error)=>{
+        Toastify({
+            text: "Oops! There was a problem submitting your form",
+            duration: 3000,
+            gravity: "top",
+            position: "right",
+            style: {
+                background: "#F44336"
+            }
+        }).showToast();
+    }).finally(()=>{
+        submitButton.disabled = false;
+        submitButton.textContent = "Submit";
+    });
+}
+form.addEventListener("submit", handleSubmit);
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting) {
+            const animationClass = entry.target.getAttribute("data-animation");
+            entry.target.classList.add(animationClass);
+        } else {
+            const animationClass = entry.target.getAttribute("data-animation");
+            entry.target.classList.remove(animationClass);
+        }
+    });
+});
+const animateElements = document.querySelectorAll("[data-animation]");
+animateElements.forEach((element)=>{
+    console.log(element);
+    observer.observe(element);
+});
+
+//# sourceMappingURL=index.fc2f7771.js.map
